efcb24aecf78dece3c1cbd659faa0d4683182b9a
Konstantin Vyatkin <kostiako2@yandex.ru>
2023-04-05 12:44:42 +0300

    Добавлено удаление, добавление и лайк карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
 const popupOpenButtonElement = document.querySelector('.profile__button-border') ;
 const popupOpenButtonElementPicture = document.querySelector('.profile__add-button-border');
 const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button')
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const itemTemplate = document.querySelector(".item_template").content;
+const groupCard = document.querySelector(".group");
+let likeButton = document.querySelectorAll(".group__like");
+let deleteButton = document.querySelectorAll(".group__trash");
 let formElement = popupElement.querySelector('.popup__form');
 let nameInput = popupElement.querySelector('.popup__input_type_name');
 let jobInput = popupElement.querySelector('.popup__input_type_job');
 let nameOutput = document.querySelector('.profile__title');
 let jobOutput = document.querySelector('.profile__subtitle');
+let cardTitle = document.querySelector('.group__title');
+let photo = document.querySelectorAll(".group__photo");
+let count = 0;
+
 
 
 
@@ -36,12 +44,19 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
-const photo = document.querySelectorAll(".group__photo");
+  
 for (let i = 0; i < initialCards.length; i++) {
     photo[i].src = initialCards[i].link;
 }
 
-  console.log(initialCards[0]);
+likeButton.forEach(likeButton => likeButton.addEventListener('click', function () {
+  likeButton.classList.toggle('group__like_active');
+}))
+
+deleteButton.forEach(deleteButton => deleteButton.addEventListener('click', function () {
+  let card = document.querySelector(".group__card");
+  card.remove();
+}))
 
 const openPopup = function() {
     nameInput.placeholder = 'Введите имя';
@@ -51,7 +66,7 @@ const openPopup = function() {
     popupElement.classList.add('popup_opened');
     nameInput.value = nameOutput.textContent;
     jobInput.value = jobOutput.textContent;
-
+    count = 0;
 }
 
 const openPopupPicture = function() {
@@ -62,8 +77,10 @@ const openPopupPicture = function() {
     jobInput.placeholder = 'Ссылка на картинку';
     const PopupTitle = document.querySelector('.popup__title');
     PopupTitle.textContent = 'Новое место';
+    count = 1;
 
 }
+
 
 const closePopup = function() { 
     popupElement.classList.remove('popup_opened')
@@ -73,13 +90,43 @@ popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
 
+
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    nameOutput.textContent = nameInput.value;
-    jobOutput.textContent = jobInput.value;
+    if (count === 0) {
+      nameOutput.textContent = nameInput.value;
+      jobOutput.textContent = jobInput.value; 
+  }
     closePopup();
   
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+function addCardSubmit (evt) {
+  evt.preventDefault();
+  if (count === 1) {
+    const htmlElement = itemTemplate.cloneNode(true);
+    htmlElement.querySelector(".group__title").textContent = nameInput.value;
+    htmlElement.querySelector(".group__photo").src = jobInput.value;
+    groupCard.prepend(htmlElement);
+    const like = document.querySelector(".group__like");
+    function addLike () {
+      
+      like.classList.toggle('group__like_active');
+    }
+    like.addEventListener('click', addLike);
+    const deleteTrash = document.querySelector(".group__trash");
+    function removeCard () {
+      let card = document.querySelector(".group__card");
+      card.remove();
+    }
+    deleteTrash.addEventListener('click', removeCard);
+  }
+  closePopup();
+}
+
+
+formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', addCardSubmit);
+
+
 
